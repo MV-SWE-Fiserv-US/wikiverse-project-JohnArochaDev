59ee2694fff1c17237f451b14be37be5393e8d37
John Arocha <john.arocha@clover.com>
2024-11-18 15:25:21 -0700

    added author, now need t oadd tags

--- public/react/components/ShowPage.jsx
+++ public/react/components/ShowPage.jsx
@@ -1,16 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function ShowPage({ showPage }) {
 
+    const [data, setData] = useState(null)
+
+    async function apiCall() {
+        const req = await fetch(`http://localhost:3000/api/users/${showPage.authorId}`)
+        const res = await req.json()
+        setData(res)
+    }
+
     useEffect(() => {
-        // make API call to find the user
-    }, [])
+        apiCall()
+    }, [showPage.authorId])
+
+    // find tags with API call next
 
     return (
         <>
             <h1>{showPage.title}</h1>
             <h3>{showPage.content}</h3>
             <p>{showPage.updatedAt}</p>
+            {data ? <p>Written By: {data.name}</p> : <p>Loading...</p>} {/* Show loading while data is fetching */}
         </>
     )
 }
