d25033518ee8ffd100a9f266e3fc8dde66a6e481
John Arocha <john.arocha@clover.com>
2024-11-19 09:50:52 -0700

    added delet functionality

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -4,9 +4,26 @@ export default function Page({ page, showPage, setShowPage }) {
 
   function handleClick() {
     setShowPage(page)
+    setTimeout(() => {
+      console.log('slug or no slug', page)
+    }, 1000)
+  }
+
+  function deleteClick(pageSlug) {
+    const url = `http://localhost:3000/api/wiki/${page.slug}`
+    console.log(url)
+  
+    fetch(url, {
+      method: 'DELETE'
+    })
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 300);
   }
 
   return <>
     <h3 onClick={handleClick} >{page.title}</h3>
+    <button onClick={deleteClick} >Delete</button>
   </>
 }
